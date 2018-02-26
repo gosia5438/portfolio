5f2e1c645968d87285df458515ca4a94cb9c1dd6
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-02-26 11:59:00 +0100

    clean the code

--- app/js/main.js
+++ app/js/main.js
@@ -2,98 +2,123 @@
 
 $(function () {
 
-	if($(window).scrollTop() >= 500){
-		$('#main-nav').addClass('scroll');
-	}
+    
+    // navigation - smooth scroll 
 
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
 
+        var target = this.hash;
 
-//    $('#carouselExampleControls').carousel({
-//        interval: 1600,
-//    });
-    	$(window).scroll(function(){
-		if($(window).scrollTop() >= 500 ) {
-			$('#main-nav').addClass('scroll');
-			$('#main-nav').css({'transition':'all 0.3s'})
-		} else {
-			$('#main-nav').removeClass('scroll');
-		}
-	});
-
-    //nawigacja
-
-    $("a[href^='#']").on('click', function () {
+        // scroll and show hash
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 74
-        }, 500);
+            'scrollTop': $(target).offset().top
+        }, 700, 'swing', function () {
+            window.location.hash = target;
+        });
     });
-    
 
-    
-    
-    
-    
+
+    // navigation - change color of the navbar after scroll
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 700) {
+            $('#main-nav').addClass('scroll');
+            $('#main-nav').css({
+                'transition': 'all 0.3s'
+            })
+        } else {
+            $('#main-nav').removeClass('scroll');
+        }
+    });
+
+
     // timeline
-    
-    (function() {
 
-
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
-
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
-  }
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
+    var items = $(".timeline li");
 
-})();
-  
+    function showExperience() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
     
+    //skills 
+    var progress = $(".progress");
+
+    function showSkills() {
+        for (var i = 0; i < progress.length; i++) {
+            if (isElementInViewport(progress[i])) {
+                $(this).addClass("in-view");
+                progressWidth();
+            }
+        }
+    }
     
+    // listen for events
+    $(window).bind('load resize scroll', showExperience);
+    $(window).bind('load resize scroll', showSkills);
+
+
     //progress bar
-    
-    $('.html').animate({width:'85%'},2000);
-    $('.css').animate({width:'75%'},2000);
-    $('.js').animate({width:'20%'},2000);
-    $('.git').animate({width:'50%'},2000);
-    $('.sass').animate({width:'65%'},2000);
-    $('.rwd').animate({width:'70%'},2000);
-    $('.wp').animate({width:'20%'},2000);
-    $('.ps').animate({width:'75%'},2000);
-    $('.id').animate({width:'40%'},2000);
-    
-    
-  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    function progressWidth() {
+        $('.html').animate({
+            width: '75%'
+        }, 2000);
+        $('.css').animate({
+            width: '75%'
+        }, 2000);
+        $('.js').animate({
+            width: '20%'
+        }, 2000);
+        $('.git').animate({
+            width: '50%'
+        }, 2000);
+        $('.sass').animate({
+            width: '65%'
+        }, 2000);
+        $('.rwd').animate({
+            width: '70%'
+        }, 2000);
+        $('.wp').animate({
+            width: '20%'
+        }, 2000);
+        $('.ps').animate({
+            width: '75%'
+        }, 2000);
+        $('.id').animate({
+            width: '40%'
+        }, 2000);
+    }
+
+//    scroll to top
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('.scroll-top').fadeIn(500);
+        } else {
+            $('.scroll-top').fadeOut(500);
+        }
+    });
+
+
+    //    portfolio gallery
+
+    $(".graphic-box[data-orientation!='vertical']").twentytwenty({
+        default_offset_pct: 0.5
+    });
+
+
 });
